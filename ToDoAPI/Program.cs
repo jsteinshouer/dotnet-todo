8da67abac4938dbe7588cf0217e53e1f046aa86c
Jason Steinshouer <jason.steinshouer@gmail.com>
2024-10-10 08:28:44 -0500

    add identity framework

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -2,22 +2,29 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using ToDoApi.Data;
 using ToDoApi.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = "Data Source=ToDo.db";
 builder.Services.AddSqlite<ApplicationDBContext>(connectionString);
 
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDBContext>();
+
 var app = builder.Build();
+
+app.MapGroup("/api").MapIdentityApi<IdentityUser>();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet( "/api/todo", (ApplicationDBContext db) => db.ToDos.AsNoTracking().ToList() );
+app.MapGet( "/api/todo", (ApplicationDBContext db) => db.ToDos.AsNoTracking().ToList() ).RequireAuthorization();
 app.MapPost( "/api/todo", (ApplicationDBContext db, ToDo todo) => {
     db.ToDos.Add(todo);
     db.SaveChanges();
     return Results.Created();
-});
+}).RequireAuthorization();
 app.MapPut( "/api/todo/{id}", (ApplicationDBContext db, ToDo updateTodo, int id) => {
     var todo = db.ToDos.SingleOrDefault(p => p.Id == id);
     if (todo is null)
@@ -27,7 +34,7 @@ app.MapPut( "/api/todo/{id}", (ApplicationDBContext db, ToDo updateTodo, int id)
     db.SaveChanges();
 
     return Results.NoContent();
-});
+}).RequireAuthorization();
 app.MapDelete( "/api/todo/{id}", (ApplicationDBContext db, int id) => {
     var todo = db.ToDos.SingleOrDefault(p => p.Id == id);
     if (todo is null)
@@ -36,6 +43,6 @@ app.MapDelete( "/api/todo/{id}", (ApplicationDBContext db, int id) => {
     db.SaveChanges();
 
     return Results.NoContent();
-});
+}).RequireAuthorization();
 
 app.Run();
